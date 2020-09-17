7e897406a891439be4ec988a51595e83b87385d7
nagajothikaliappan <nagajothi.kaliappan@thewarehouse.co.nz>
2020-09-17 18:26:54 +1200

    Writing the result in excel using hashmap

--- src/test/java/com/gmail/test/pages/GoibiboFlightPage.java
+++ src/test/java/com/gmail/test/pages/GoibiboFlightPage.java
@@ -24,6 +24,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.waits.Wait;
+import net.serenitybdd.screenplay.waits.WaitUntil;
 
 public class GoibiboFlightPage extends PageObject {
 
@@ -41,7 +43,7 @@ public class GoibiboFlightPage extends PageObject {
 	List <WebElement> depTime;
 	
 	@FindBy(xpath=".//div[contains(@data-cy,'flightItem_NAV42')]//div[@data-cy='duration']")
-	List <WebElement> duration;
+	List <WebElement> duration1;
 	
 	@FindBy(xpath=".//div[contains(@data-cy,'flightItem_NAV42')]//span[@data-cy='arrTime']")
 	List <WebElement> arrival1;
@@ -50,7 +52,7 @@ public class GoibiboFlightPage extends PageObject {
 	List <WebElement> arrival2;
 	
 	@FindBy(xpath=".//div[contains(@data-cy,'flightItem_NAV42')]//span[@data-cy='finalPrice']")
-	List <WebElement> price;
+	List <WebElement> price1;
 	
 	
 	public static final By flightResult = By.xpath(".//div[contains(@data-cy,'flightItem_NAV42')]");
@@ -79,64 +81,51 @@ public class GoibiboFlightPage extends PageObject {
 	}
 	
 	public void writeInExcel() throws IOException {
-//		String filepath= (".\\src\\test\\resources\\Output\\Flights.xlsx");
 		File myFile = new File(".\\src\\test\\resources\\Output\\Flights.xlsx");
 		XSSFWorkbook wb= new XSSFWorkbook();
 		XSSFSheet sh= wb.createSheet("Flights");
 		FileOutputStream fos=new FileOutputStream(myFile);
-		XSSFRow header = sh.createRow(0);
-	    header.createCell(0).setCellValue("DEPARTURE");
-	    header.createCell(1).setCellValue("DURATION");
-	    header.createCell(2).setCellValue("ARRIVAL");
-	    header.createCell(3).setCellValue("PRICE");
+	    waitForCondition().until(ExpectedConditions.presenceOfAllElementsLocatedBy(flightResult));
 		int noOfFlights = flightResults.size();
-//		Map<String, Object[]> data = new HashMap<String, Object[]>(); 
-//		data.put("7", new Object[] {7d, "Sonya", "75K", "SALES", "Rupert"}); 
-//		data.put("8", new Object[] {8d, "Kris", "85K", "SALES", "Rupert"}); 
-//		data.put("9", new Object[] {9d, "Dave", "90K", "SALES", "Rupert"}); 
-//		// Set to Iterate and add rows into XLS file 
-//		Set<String> newRows = data.keySet(); 
-//		// get the last row number to append new data 
-//		int rownum = sh.getLastRowNum(); 
-//		for (String key : newRows) { 
-//			// Creating a new Row in existing XLSX sheet 
-//			Row row = sh.createRow(rownum++); 
-//			Object [] objArr = data.get(key); 
-//			int cellnum = 0; 
-//			for (Object obj : objArr) { 
-//				Cell cell = row.createCell(cellnum++); 
-//				if (obj instanceof String) { 
-//					cell.setCellValue((String) obj); 
-//					} 
-//				else if (obj instanceof Boolean) { 
-//					cell.setCellValue((Boolean) obj); 
-//					} 
-//				else if (obj instanceof Date) { 
-//					cell.setCellValue((Date) obj); 
-//					} 
-//				else if (obj instanceof Double) { 
-//					cell.setCellValue((Double) obj); 
-//					} 
-//				} 
-//			} // open an OutputStream to save written data into XLSX file 
-//		FileOutputStream os = new FileOutputStream(myFile); 
-//		wb.write(os); 
-//		System.out.println("Writing on XLSX file Finished ...");
-//		}
-	for(int j=0; j <= 4; j++ ) {
-			for(int i = 0; i <= noOfFlights-1; i++) {
-				XSSFRow rw= sh.createRow(i);
-				XSSFCell cl=rw.createCell(j);
-				cl.setCellType(CellType.STRING);
-				String departure = depTime.get(i).getText().toString();
-	            System.out.println(departure);
-	            cl.setCellValue(departure);
-			}
-			
+		Map<Integer, Object[]> data = new HashMap<Integer, Object[]>(); 
+		data.put(0, new Object[] {"DEPARTURE", "DURATION", "ARRIVAL", "PRICE"});
+		for(int i =0; i <= noOfFlights-1; i++ ) {
+			String departure = depTime.get(i).getText().toString();
+			String duration = duration1.get(i).getText().toString();
+			String arrival = arrival1.get(i).getText().toString();
+			String price = price1.get(i).getText().toString();
+			data.put(i+1, new Object[] {departure, duration, arrival, price});
+			waitForCondition().until(ExpectedConditions.presenceOfAllElementsLocatedBy(flightResult));
+			noOfFlights = flightResults.size();			
 		}
-		fos.flush();     
-		wb.write(fos);     
-		fos.close();
 		
-	}
+		// Set to Iterate and add rows into XLS file 
+		Set<Integer> newRows = data.keySet(); 
+		// get the last row number to append new data 
+		int rownum = sh.getLastRowNum(); 
+		for (Integer key : newRows) { 
+			// Creating a new Row in existing XLSX sheet 
+			Row row = sh.createRow(rownum++); 
+			Object [] objArr = data.get(key); 
+			int cellnum = 0; 
+			for (Object obj : objArr) { 
+				Cell cell = row.createCell(cellnum++); 
+				if (obj instanceof String) { 
+					cell.setCellValue((String) obj); 
+					} 
+				else if (obj instanceof Boolean) { 
+					cell.setCellValue((Boolean) obj); 
+					} 
+				else if (obj instanceof Date) { 
+					cell.setCellValue((Date) obj); 
+					} 
+				else if (obj instanceof Double) { 
+					cell.setCellValue((Double) obj); 
+					} 
+				} 
+			} // open an OutputStream to save written data into XLSX file 
+		FileOutputStream os = new FileOutputStream(myFile); 
+		wb.write(os); 
+		System.out.println("Writing on XLSX file Finished ...");
+		}
 }
